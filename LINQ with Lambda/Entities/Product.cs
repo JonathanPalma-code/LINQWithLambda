63f8906b2b322f66073e4f0f16f485691a61e6e1
Jonathan <jonathanpalma89@gmail.com>
2020-03-27 15:47:07 +0000

    chore: add Entities (Product and Category Classes)

---
+++ LINQ with Lambda/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_with_Lambda.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}\nName: {Name}\nPrice: {Price.ToString("F2")}, Category name: {Category.Name}, Category Tier: {Category.Tier}";
+        }
+    }
+}
