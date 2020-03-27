da41e0d714ac0bf820ee7c960167ca80d5d53079
Jonathan <jonathanpalma89@gmail.com>
2020-03-27 15:38:48 +0000

    chore: add new project

---
+++ LINQ with Lambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_with_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
